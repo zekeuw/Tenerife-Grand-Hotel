8191bdc60c2aba9bb46dab7996cfbd22ffcda9c9
Alberto <67468673+Alberto-72@users.noreply.github.com>
2026-01-25 22:24:30 +0000

    Added photo carrusel, footer, and improved the top bar menu responsive

--- src/Frontend/main.py
+++ src/Frontend/main.py
@@ -1,13 +1,25 @@
 import flet as ft
-
+from all_rooms import AllRoomsView
 
 def main(page: ft.Page):
+    VIEWS_WIDTH = page.width *0.8
+    VIEWS_HEIGTH = 800
+
+    photo_list = [
+        ft.Image(src="../../media/img/photo_views/photo1.jpg", border_radius=15,width=VIEWS_WIDTH, height=VIEWS_HEIGTH, fit="COVER"),
+        ft.Image(src="../../media/img/photo_views/photo2.jpg", border_radius=15,width=VIEWS_WIDTH, height=VIEWS_HEIGTH, fit="COVER"),
+        ft.Image(src="../../media/img/photo_views/photo3.jpg", border_radius=15,width=VIEWS_WIDTH, height=VIEWS_HEIGTH, fit="COVER"),
+        ft.Image(src="../../media/img/photo_views/photo4.jpg", border_radius=15,width=VIEWS_WIDTH, height=VIEWS_HEIGTH, fit="COVER"),
+        ft.Image(src="../../media/img/photo_views/photo5.jpg", border_radius=15,width=VIEWS_WIDTH, height=VIEWS_HEIGTH, fit="COVER")
+    ]
     page.title = "Tenerife Grand Hotel"
     page.bgcolor = "white"
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.horizontal_alignment = ft.MainAxisAlignment.START
-    page.padding = 40
+    page.scroll = ft.ScrollMode.AUTO
+    page.padding = 0
 
+    # TOP BAR
     nombre =  ft.Text(
         value="Tenerife\nGrand Hotel", 
         size=20 ,color="black", 
@@ -17,10 +29,15 @@ def main(page: ft.Page):
         value="Mis reservas", 
         color="black",size=18 ,
         align=ft.Alignment.TOP_LEFT)
-    habitaciones = ft.Text(
-        value="Habitaciones", 
-        color="black", size=18 ,
-        align=ft.Alignment.TOP_LEFT)
+    habitaciones = ft.TextButton(
+        content="Habitaciones",
+        style=ft.ButtonStyle(
+            color="black",  # Texto negro
+            text_style=ft.TextStyle(size=18), # Tamaño 18
+            overlay_color="transparent" # Opcional: quita el efecto gris al pasar el mouse si no lo quieres
+        ),
+        on_click=lambda _: page.push_route("/all_rooms")
+    )
 
     login = ft.Button(
         content="Log in", 
@@ -35,16 +52,41 @@ def main(page: ft.Page):
             side=ft.BorderSide(width=1, color=ft.Colors.BLUE),
             color="blue"
         )
-    )
+    ) 
+    
+    top_var_container_mobile = ft.Container(
+        width=float("inf"),
+        height=float("inf"),
+        bgcolor="white",
+        visible=False,
+        content=ft.Column(
+            controls=[
+                ft.TextButton("Inicio"),
+                ft.TextButton("Mis Reservas"),
+                ft.TextButton("Habitaciones"),
 
-    icon_menu = ft.Image(
-        src= "../../media/img/icon_menu.png",
-        height=50,
-        width=50,
-        align=ft.Alignment.TOP_RIGHT
+                ft.ElevatedButton("Log in", bgcolor="blue", color="white", width=float("inf")),
+                ft.OutlinedButton("Sign up", width=float("inf"))
+            ]
+        )
+    )
+    
+    def openMobileMenu(e):
+        top_var_container_mobile.visible = True
+        page.update()
+
+    icon_menu =ft.IconButton(
+            icon = ft.Image(
+                src= "../../media/icons/icon_menu.png",
+                height=50,
+                width=50,    
+                ),   
+            on_click = openMobileMenu
     )
 
     top_bar_container = ft.Container(
+        padding=30,
+        content=
         ft.Row(controls=[
             ft.Row(
                 controls=[
@@ -63,11 +105,27 @@ def main(page: ft.Page):
         )
     )
     
+
+    # COVER
+    cover_text_title = ft.Text(
+                        value="Bienvenido al paraiso",
+                        size=40,
+                        color="white",
+                        weight="bold",
+                        text_align="center"
+                    )
+    cover_text_description = ft.Text(
+                        value="Encuentra tu habitacion perfecta, hecha para ti",
+                        size=15,
+                        color="white",
+                        weight="bold",
+                        text_align="center"
+                    )
     cover_img_container = ft.Container(
+        margin=ft.margin.only(left=20, right=20, bottom=50),
         border_radius=20,
         width=float("inf"),
         height=300,
-        margin=ft.margin.only(bottom=50),
         image=ft.DecorationImage(
             src= "../../media/img/cover_photo.jpg",
             fit = "COVER",
@@ -76,20 +134,8 @@ def main(page: ft.Page):
         content=ft.Container(
             ft.Column(
                 controls=[
-                    ft.Text(
-                        value="Bienvenido al paraiso",
-                        size=40,
-                        color="white",
-                        weight="bold",
-                        text_align="center"
-                    ),
-                    ft.Text(
-                        value="Encuentra tu habitacion perfecta, hecha para ti",
-                        size=15,
-                        color="white",
-                        weight="bold",
-                        text_align="center"
-                    )
+                    cover_text_title,
+                    cover_text_description
                 ],
                 alignment=ft.MainAxisAlignment.CENTER, 
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -100,7 +146,7 @@ def main(page: ft.Page):
         )
     )
 
-    
+    # BEST ROOMS
     hab = {
         "Luxury": {
             "LX_1": {
@@ -201,7 +247,6 @@ def main(page: ft.Page):
     # When the backend is done, instead of range(10), it will be a list of json objects,
     # as same as the CreateRoomCard where the info will depend on that list
     
-    for data in hab["Luxury"]: print(data,"\n\n")
     cards_list = [CreateRoomCard(hab["Luxury"][data]) for data in hab["Luxury"]]
 
     card_carrusel = ft.Row(
@@ -212,10 +257,10 @@ def main(page: ft.Page):
     )
 
     desplazamiento = 340 * 2
-    async def scroll_izquierda(e):
+    async def leftCardScroll(e):
         await card_carrusel.scroll_to(delta=(-desplazamiento), duration=500, curve="easeOut")
 
-    async def scroll_derecha(e):
+    async def rigthCardScroll(e):
         await card_carrusel.scroll_to(delta=desplazamiento, duration=500, curve="easeOut")
 
     btn_lef= ft.Container(
@@ -225,7 +270,7 @@ def main(page: ft.Page):
             src="../../media/icons/icon_left.png",
             fit="COVER"
         ),
-        on_click= scroll_izquierda
+        on_click= leftCardScroll
     )
 
     btn_rig = ft.Container(
@@ -235,10 +280,11 @@ def main(page: ft.Page):
             src="../../media/icons/icon_right.png",
             fit="COVER"
             ),
-        on_click= scroll_derecha
+        on_click= rigthCardScroll
     )
 
     best_valued_rooms_container = ft.Container(
+        margin=ft.margin.only(right=30, left=30),
         width=float("inf"),
         height=350,
         content=ft.Container(
@@ -256,6 +302,7 @@ def main(page: ft.Page):
                                 align=ft.Alignment.CENTER_LEFT,
                                 padding=10,
                                 width=60,
+                                margin=ft.margin.only(right=10)
                                 ),
                             card_carrusel,
                             ft.Container(
@@ -263,6 +310,7 @@ def main(page: ft.Page):
                                 align=ft.Alignment.CENTER_LEFT,
                                 padding=10,
                                 width=60,
+                                margin=ft.margin.only(left=10)
                                 ),
                             ]
                         )
@@ -270,37 +318,171 @@ def main(page: ft.Page):
             )
         )
     )
-    
 
+    
+    photo_carrusel = ft.Row(
+        expand=True,
+        controls=photo_list,
+        scroll = ft.ScrollMode.HIDDEN,
+        
+        spacing=0,
+    )
+    
+    async def leftPhotoScroll(e):
+        await photo_carrusel.scroll_to(delta=-VIEWS_WIDTH, duration=600, curve="easeOut")
+
+    async def RigthPhotoScroll(e):
+        await photo_carrusel.scroll_to(delta=VIEWS_WIDTH, duration=600, curve="easeOut")
+
+    btn_photo_lef= ft.Container(
+        height=40,
+        expand=True,
+        content = ft.Image(
+            src="../../media/icons/icon_left.png",
+            fit="COVER"
+        ),
+        on_click= leftPhotoScroll
+    )
+
+    btn_photo_rig = ft.Container(
+        height=40,
+        expand=True,
+
+        content = ft.Image(
+            src="../../media/icons/icon_right.png",
+            fit="COVER"
+            ),
+        on_click= RigthPhotoScroll
+    )
+
+    views_container = ft.Container(
+        height=VIEWS_HEIGTH,
+        width=float("inf"),
+        alignment=ft.Alignment.CENTER,
+        margin=ft.margin.only(top=80),
+        
+        content = ft.Container(
+                    height=VIEWS_HEIGTH,
+                    width=VIEWS_WIDTH,
+                    alignment=ft.Alignment.CENTER,
+                    content=ft.Column(
+                        controls=[
+                            ft.Text(
+                                    value="Bienvenido a tu paraiso, Bienvenido a Tenerife",
+                                    color=ft.Colors.BLACK,
+                                    weight="bold",
+                                    size=25
+                                ),
+                            ft.Container(
+                                shadow= ft.BoxShadow(
+                                    blur_radius=10,
+                                    color=ft.Colors.BLACK,
+                                ),
+                                content=ft.Stack(
+                                        alignment=ft.Alignment.CENTER,
+                                        controls=[
+                                            photo_carrusel,
+                                            
+                                            ft.Container(
+                                                content=btn_photo_lef,
+                                                left=10,  
+                                                top=380,
+                                                bgcolor=ft.Colors.WHITE_30,
+                                                ),
+                                            
+                                            ft.Container(
+                                                content=btn_photo_rig,
+                                                right=10,     
+                                                top=380,
+                                                bgcolor=ft.Colors.WHITE_30
+                                                ),
+                                            ],
+                                    )
+                            ),
+                            
+                        ]
+                    )
+                        
+                    
+                )
+    )
+
+    footer_container = ft.Container(
+        margin=ft.margin.only(top=100),
+        padding=ft.padding.only(left=30, right=30, top=20, bottom=20),
+        bgcolor=ft.Colors.BLACK_12,
+        content=ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                ft.Column(
+                    controls=[
+                        ft.Text(
+                            value= "Tenerife Grand Hotel",
+                            weight="bold",
+                            color=ft.Colors.BLACK,
+                            size=18
+                        ),
+                        ft.Text(
+                            value= "El paraiso en la tierra solo para ti\ndesde 2006",
+                            color=ft.Colors.BLACK,
+                            size=18
+                        ),
+                    ],
+                    alignment=ft.Alignment.CENTER_LEFT
+                ),
+                ft.Column(
+                    controls=[
+                        ft.Text(
+                            value= "Ayuda",
+                            weight="bold",
+                            color=ft.Colors.BLACK,
+                            size=18,
+                            text_align=ft.TextAlign.END
+                        ),
+                        ft.Text(
+                            value= "FAQ",
+                            color=ft.Colors.BLACK,
+                            size=18,
+                            text_align=ft.TextAlign.END
+                        ),
+                        ft.Text(
+                            value= "Contactanos",
+                            color=ft.Colors.BLACK,
+                            size=18,
+                            text_align=ft.TextAlign.END
+                        ),
+                    ],
+
+
+                )
+            ]
+        )
+    )
     icon_menu.visible = False
 
     # Addings
     page.add(top_bar_container)
     page.add(cover_img_container)
     page.add(best_valued_rooms_container)
-
-
+    page.add(views_container)
+    page.add(footer_container)
+    
+    page.overlay.append(top_var_container_mobile)
 
     def responsive(e):
-        # Modo móvil
-        if page.width < 800:
-            print("estoy en movil")
-            login.visible = False
-            signup.visible = False
-            habitaciones.visible = False
-            reservas.visible = False
-            icon_menu.visible = True
-            page.padding = 20
-        # Modo PC
-        else:
-            login.visible = True
-            signup.visible = True
-            habitaciones.visible = True
-            reservas.visible = True
-            icon_menu.visible = False
-            page.padding = 40
+        is_mobile = page.width < 800
+        
+        signup.visible = False if is_mobile else True
+        login.visible = False if is_mobile else True
+        reservas.visible = False if is_mobile else True
+        habitaciones.visible = False if is_mobile else True
+        
+        icon_menu.visible = True if is_mobile else False
+
         page.update()
 
     page.on_resize = responsive
+    responsive(None)
 
 ft.app(main)
