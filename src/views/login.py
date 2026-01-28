97837fb4aa6ce5604685e96646e8618f4d8f6161
Pablo Rodríguez Martín <91605273+PabloRodMar@users.noreply.github.com>
2026-01-28 13:07:57 +0000

    Login (frontend) diseñado

--- src/views/login.py
+++ src/views/login.py
@@ -1,5 +1,9 @@
-import flet as ft
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
+import flet as ft
+from src.components.navigation_bar import NavigationBar
 
 def main(page: ft.Page):
     page.title = "Tenerife Grand Hotel"
@@ -8,84 +12,33 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.MainAxisAlignment.START
     page.padding = 20
 
-    nombre =  ft.Text(
-        value="Tenerife\nGrand Hotel", 
-        size=20 ,color="black", 
-        weight="bold", 
-        align=ft.Alignment.TOP_LEFT)
-
-    login = ft.Button(
-        content="Log in", 
-        align=ft.Alignment.TOP_RIGHT,
-        color="white",
-        bgcolor="blue"
-        )
-    
-    signup = ft.OutlinedButton(
-        content="Sign up", 
-        align=ft.Alignment.TOP_RIGHT,
-        style=ft.ButtonStyle(
-            side=ft.BorderSide(width=1, color=ft.Colors.BLUE),
-            color="blue"
-        )
-    )
-
-    icon_menu = ft.Image(
-        src= "/icons/icon_menu.png",
-        height=50,
-        width=50,
-        align=ft.Alignment.TOP_RIGHT
-    )
-
-    top_bar_container = ft.Container(
-        ft.Row(controls=[
-            ft.Row(
-                controls=[
-                    nombre,
-                ],
-                spacing=50  
-            ),
-            ft.Row(controls=[
-                signup,
-                login
-            ])
-        ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-        )
-    )
+    menu = NavigationBar(page)
 
     central_container = ft.Container(
-        content=ft.Column(
-            [
-                ft.Text("Bienvenido al Lujo", size=30, weight="bold", color="black"),
-                ft.Text("Tu estancia en Tenerife empieza aquí", color="grey")
-            ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            alignment=ft.MainAxisAlignment.CENTER,
+        expand=True,
+        content= ft.Container(
+            content=ft.Column(
+                [
+                    ft.Text("Tu estancia en Tenerife empieza aquí", size=30, weight="bold", color="black"),
+                    ft.Text("Login", size=30, color="black"),
+                    ft.TextField(label="Username", color="#000000", hint_text="Introduzca su nombre de usuario..."),
+                    ft.Text("Registro", size=30, color="black"),
+                    ft.TextField(label="Contrasena", color="black", hint_text="Introduzca su contraseña...", password=True),
+                    ft.Button(content="Iniciar sesión", color="white", bgcolor="blue", scale=1.5, margin=10)
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            width=page.width,
+            height=page.height*0.75,
+            bgcolor="#d1d1d1",
+            border_radius=10,
         ),
-        alignment=ft.Alignment(0, 0), 
-        expand=True, 
-        bgcolor="#f5f5f5",
-        border_radius=10,
+        alignment=ft.Alignment.CENTER, 
     )
+   
 
-    def responsive(e):
-        # Modo móvil
-        if page.width < 400:
-            print("estoy en movil")
-            login.visible = False
-            signup.visible = False
-            icon_menu.visible = True
-            page.padding = 20
-        # Modo PC
-        else:
-            login.visible = True
-            signup.visible = True
-            page.padding = 20
-        page.update()
-
-    page.on_resize = responsive
-
-    page.add(top_bar_container)
+    page.add(menu)
     page.add(central_container)
 
 ft.app(main)
