9c3acc7e15c850f33f9a936ab6f842b8bfae9f6e
Pablo Rodríguez Martín <91605273+PabloRodMar@users.noreply.github.com>
2026-01-28 22:43:35 +0000

    Signup (beta)

--- src/views/signup.py
+++ src/views/signup.py
@@ -1,133 +1,79 @@
-import flet as ft
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
+import flet as ft
+from src.components.navigation_bar import NavigationBar
 
 def main(page: ft.Page):
-    VIEWS_WIDTH = page.width * 0.8
-    VIEWS_HEIGTH = 800
-
+    page.title = "Tenerife Grand Hotel"
     page.bgcolor = "white"
+    page.vertical_alignment = ft.MainAxisAlignment.START
+    page.horizontal_alignment = ft.MainAxisAlignment.START
+    page.padding = 20
 
-    nombre =  ft.Text(
-        value="Tenerife\nGrand Hotel", 
-        size=20 ,color="black", 
-        weight="bold", 
-        align=ft.Alignment.TOP_LEFT
-    )
+    menu = NavigationBar(page)
 
-    login = ft.Button(
-        content="Log in", 
-        align=ft.Alignment.TOP_RIGHT,
-        color="white",
-        bgcolor="blue"
-    )
-    
-    btnsignup = ft.OutlinedButton(
-        content="Sign up", 
-        align=ft.Alignment.TOP_RIGHT,
-        style=ft.ButtonStyle(
-            side=ft.BorderSide(width=1, color=ft.Colors.BLUE),
-            color="blue"
-        )
-    )
-
-    top_var_container_mobile = ft.Container(
-        width=float("inf"),
-        height=float("inf"),
-        bgcolor="white",
-        visible=False,
-        content=ft.Column(
-            controls=[
-                ft.ElevatedButton("Log in", bgcolor="blue", color="white", width=float("inf")),
-                ft.OutlinedButton("Sign up", width=float("inf"))
-            ]
-        )
-    )
-
-    def openMobileMenu(e):
-        top_var_container_mobile.visible = True
-        page.update()
-
-    icon_menu =ft.IconButton(
-            icon = ft.Image(
-                src= "../../media/icons/icon_menu.png",
-                height=50,
-                width=50,    
-                ),   
-            on_click = openMobileMenu
-    )
-
-    top_bar_container = ft.Container(
-        padding=30,
-        content=
-        ft.Row(controls=[
-            ft.Row(
-                controls=[
-                    nombre
+    central_container = ft.Container(
+        expand=True,
+        content= ft.Container(
+            content=ft.Column(
+                [
+                    ft.Text("🌴 Tu estancia en Tenerife empieza aquí 🌴", size=30, weight="bold", color="black"),
+                    ft.Text("Registro", size=30, weight="bold", color="black"),
+                    ft.TextField(
+                        label="Nombre de usuario", 
+                        color="#000000",
+                        hint_text="Introduzca su nombre de usuario...", 
+                        label_style=ft.TextStyle(color=ft.Colors.BLACK), 
+                        hint_style=ft.TextStyle(color=ft.Colors.BLACK),
+                        focused_border_color="black",
+                        margin=15
+                        ),
+                    ft.TextField(
+                        label="Correo electrónico", 
+                        color="#000000",
+                        hint_text="Introduzca su correo...", 
+                        label_style=ft.TextStyle(color=ft.Colors.BLACK), 
+                        hint_style=ft.TextStyle(color=ft.Colors.BLACK),
+                        focused_border_color="black",
+                        margin=15
+                        ),
+                    ft.TextField(
+                        label="Contraseña",
+                        color="black",
+                        hint_text="Introduzca su contraseña...", 
+                        password=True,
+                        label_style=ft.TextStyle(color=ft.Colors.BLACK), 
+                        hint_style=ft.TextStyle(color=ft.Colors.BLACK),
+                        focused_border_color="black",
+                        margin=15
+                        ),
+                    ft.TextField(
+                        label="Repita la contraseña",
+                        color="black",
+                        hint_text="Introduzca de nuevo su contraseña...", 
+                        password=True,
+                        label_style=ft.TextStyle(color=ft.Colors.BLACK), 
+                        hint_style=ft.TextStyle(color=ft.Colors.BLACK),
+                        focused_border_color="black",
+                        margin=15
+                        ),
+                    ft.Button(content="Crear cuenta", color="white", bgcolor="blue", scale=1.5, margin=10),
                 ],
-                spacing=50  
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                alignment=ft.MainAxisAlignment.CENTER,
             ),
-            ft.Row(controls=[
-                btnsignup,
-                login,
-                icon_menu
-            ])
-        ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-        )
+            width=page.width,
+            height=page.height,
+            bgcolor="#d1d1d1",
+            border_radius=10,
+        ),
+        alignment=ft.Alignment.CENTER, 
     )
+   
 
-    cabecera = ft.Text(
-        value="Registro de usuario", 
-        size=20 ,color="black", 
-        weight="bold", 
-        align=ft.Alignment.CENTER
-    )
-
-    full_name = ft.TextField (
-        hint_text="Introduzca su nombre completo...",
-        color="black",
-        align=ft.Alignment.TOP_LEFT
-    )
-
-    location = ft.TextField (
-        hint_text="Introduzca su pais...",
-        color="black",
-        align=ft.Alignment.TOP_LEFT
-    )
-
-    signup = ft.Container(
-        padding=30,
-        bgcolor="#f6f6f6",
-        width=100,
-        content=
-        ft.Row(controls=[
-            ft.Column(
-                controls=[
-                    cabecera,
-                    full_name,
-                    location
-                ],
-                spacing=50  
-            )
-        ], alignment=ft.Alignment.CENTER
-        )
-    )
-
-    # Addings
-    page.add(top_bar_container)
-    page.add(signup)
-    page.overlay.append(top_var_container_mobile)
-
-    def responsive(e):
-        is_mobile = page.width < 800
-        
-        btnsignup.visible = False if is_mobile else True
-        login.visible = False if is_mobile else True
-        
-        icon_menu.visible = True if is_mobile else False
-
-        page.update()
-
-    page.on_resize = responsive
-    responsive(None)
+    page.add(menu)
+    page.add(central_container)
 
 ft.app(main)
