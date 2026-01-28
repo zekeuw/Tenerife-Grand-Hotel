d0503feb2806b9ad4e24d78601c5cf1d992ea4d9
zeke <gaelverar@gmail.com>
2026-01-28 12:10:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 from src.views.home_page import homePage
 from src.views.all_rooms import allRooms
+from src.views.user_page import userPage
 
 def main(page: ft.Page):
     page.title = "Tenerife Grand Hotel"
@@ -9,6 +10,8 @@ def main(page: ft.Page):
         print(f"Ruta actual: {page.route}") # Debug para ver qué pasa
         page.views.clear() if page.views  else None
         page.views.append(homePage(page))
+        if page.route == "/userPage":
+            page.views.append(userPage(page)) # Pa saltar directamente a users en debug
         if page.route == "/allRooms":
             page.views.append(allRooms(page))
         page.update()
