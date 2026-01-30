7e49917b1d683d5dcc3662a06fc107148ca53051
zeke <gaelverar@gmail.com>
2026-01-30 13:52:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.views.single_room import singleRoom
 
 def main(page: ft.Page):
     page.title = "Tenerife Grand Hotel"
-    page.username = "luis02"
+    page.username = None
 
     
     def route_change(route):
